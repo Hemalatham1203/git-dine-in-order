50c68ed4bd1a178a6fdbf0efaaa63fd8e397e6c5
Hemalatham1203 <hemalatham1203@gmail.com>
2025-02-27 16:47:30 +0530

    modified controller to reduce redundancy

--- src/main/java/com/example/dio/controller/UserController.java
+++ src/main/java/com/example/dio/controller/UserController.java
@@ -2,6 +2,7 @@ package com.example.dio.controller;
 
 import com.example.dio.model.User;
 import com.example.dio.service.UserService;
+import com.example.dio.util.ResponseBuilder;
 import com.example.dio.util.ResponseStructure;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -17,14 +18,12 @@ public class UserController {
 
     private final UserService userService;
     @PostMapping("/register")
-    public ResponseEntity<ResponseStructure<User>> registerUser(@RequestBody User user){
+    public ResponseEntity<ResponseStructure<String>> registerUser(@RequestBody User user){
         user=userService.registerUser(user);
 
-        ResponseStructure<User> structure=new ResponseStructure<>();
-        structure.setData(user);
-        structure.setStatus(HttpStatus.CREATED.value());
-        structure.setMessage("User Created");
+        return ResponseBuilder.success(HttpStatus.CREATED,"User created","user");
 
-        return new ResponseEntity<ResponseStructure<User>>(structure,HttpStatus.CREATED);
     }
+
+
 }
