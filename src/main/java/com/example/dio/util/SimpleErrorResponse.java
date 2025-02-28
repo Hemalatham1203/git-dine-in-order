5b28656f36c3542eecb06c982107e7093a7a5f3b
Hemalatham1203 <hemalatham1203@gmail.com>
2025-02-28 12:54:36 +0530

    updated to find the user by id

---
+++ src/main/java/com/example/dio/util/SimpleErrorResponse.java
@@ -0,0 +1,12 @@
+package com.example.dio.util;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class SimpleErrorResponse {
+    private String type;
+    private int status;
+    private String message; // failed to update the user, The user is not found by the id
+}
