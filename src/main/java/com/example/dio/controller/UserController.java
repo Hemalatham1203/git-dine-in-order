f8cbe73be994c701204c2773c14e74dc336ea801
Hemalatham1203 <hemalatham1203@gmail.com>
2025-03-03 12:24:26 +0530

    updated to update the existing user

--- src/main/java/com/example/dio/controller/UserController.java
+++ src/main/java/com/example/dio/controller/UserController.java
@@ -1,10 +1,14 @@
 package com.example.dio.controller;
 
+import com.example.dio.dto.request.RegistrationRequest;
+import com.example.dio.dto.request.UserRequest;
+import com.example.dio.dto.response.UserResponse;
 import com.example.dio.model.User;
 import com.example.dio.service.UserService;
 import com.example.dio.util.ResponseBuilder;
 import com.example.dio.util.ResponseStructure;
 import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,18 +22,22 @@ public class UserController {
     private final UserService userService;
 
     @PostMapping("/register")
-    public ResponseEntity<ResponseStructure<String>> registerUser(@RequestBody User user) {
-        user = userService.registerUser(user);
-        return ResponseBuilder.success(HttpStatus.CREATED, "User created", "user");
+    public ResponseEntity<ResponseStructure<UserResponse>> registerUser(@RequestBody RegistrationRequest registrationRequest) {
+        UserResponse response = userService.registerUser(registrationRequest);
+        return ResponseBuilder.success(HttpStatus.CREATED, "User created",response);
     }
 
     @GetMapping("/users/{userId}")
-    public ResponseEntity<ResponseStructure<User>> findUserById(@PathVariable long userId) {
-        User user=userService.findUserById(userId);
-        return ResponseBuilder.success(HttpStatus.OK,"user Found", user);
+    public ResponseEntity<ResponseStructure<UserResponse>> findUserById(@PathVariable long userId) {
+        UserResponse response=userService.findUserById(userId);
+        return ResponseBuilder.success(HttpStatus.OK,"user Found", response);
     }
 
-
+    @PutMapping("/users/update/{userId}")
+    public ResponseEntity<ResponseStructure<UserResponse>> updateUserById(@RequestBody UserRequest userRequest,@PathVariable long userId){
+        UserResponse response=userService.updateUserById(userRequest,userId);
+        return ResponseBuilder.success(HttpStatus.OK,"User Updated", response);
+    }
 }
 
 
