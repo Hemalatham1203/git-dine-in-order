1fc9fa8634894760a7a6a84b237725e81eb4d631
Hemalatham1203 <hemalatham1203@gmail.com>
2025-03-03 12:20:51 +0530

    User service updated

--- src/main/java/com/example/dio/service/UserService.java
+++ src/main/java/com/example/dio/service/UserService.java
@@ -1,13 +1,16 @@
 package com.example.dio.service;
 
+import com.example.dio.dto.request.RegistrationRequest;
+import com.example.dio.dto.request.UserRequest;
+import com.example.dio.dto.response.UserResponse;
 import com.example.dio.model.User;
 import org.springframework.stereotype.Service;
 
 @Service
 public interface UserService {
-    User registerUser(User user);
+   public UserResponse  registerUser(RegistrationRequest registrationRequest);
 
-    User findUserById(long userId);
+    UserResponse findUserById(long userId);
 
-
+    UserResponse updateUserById(UserRequest userRequest,long userId);
 }
