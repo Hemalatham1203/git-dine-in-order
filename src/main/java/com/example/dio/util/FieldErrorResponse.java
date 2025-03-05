5df09d44cec523b3e5733d8389f1aea54effb89f
Hemalatham1203 <hemalatham1203@gmail.com>
2025-03-05 15:31:31 +0530

    updated with  documentation

--- src/main/java/com/example/dio/util/FieldErrorResponse.java
+++ src/main/java/com/example/dio/util/FieldErrorResponse.java
@@ -10,6 +10,13 @@ import java.util.List;
 public class FieldErrorResponse extends SimpleErrorResponse {
     private List<FieldError> errors;
 
+    /**
+     *Creates and returns new FieldError object with the specified error message, rejected value, and field name
+     * @param message error message describing the validation issue.
+     * @param rejectedValue  that was rejected or failed validation
+     * @param field name of the field where the validation error occurred.
+     * @return new FieldError object containing the provided error details (message, rejected value, and field name).
+     */
     public static FieldError createFieldError(String message,Object rejectedValue,String field){
         FieldError error=new FieldError();
         error.message=message;
--- src/main/java/com/example/dio/util/ResponseBuilder.java
+++ src/main/java/com/example/dio/util/ResponseBuilder.java
@@ -6,6 +6,14 @@ import org.springframework.http.ResponseEntity;
 
 public class ResponseBuilder {
 
+    /**
+     * Helps creating the success response with data including the Http status code, message and data itself.
+     *
+     * @param status  the status  of the request.
+     * @param message the message to the user.
+     * @param data the data involved in the operation.
+     * @return  ResponseEntity of type ResponseStructure or Type T(The type of data).
+     */
     public static <T> ResponseEntity<ResponseStructure<T>> success(HttpStatus status,String message,T data) {
         ResponseStructure<T> structure=ResponseStructure.<T>builder()
                 .status(status.value())
@@ -27,6 +35,12 @@ public class ResponseBuilder {
                 .body(structure);
     }
 
+    /**
+     * helps to creating the error response with data include http status and message
+     * @param status to return the status code
+     * @param message to error message which include in the response
+     * @return a responseEntity containing a Simple error Object
+     */
     public static ResponseEntity<SimpleErrorResponse> error(HttpStatus status, String message){
         SimpleErrorResponse error=SimpleErrorResponse.builder()
                 .type(status.name())
@@ -36,5 +50,34 @@ public class ResponseBuilder {
 
         return ResponseEntity.status(status)
                 .body(error);
+    }
+
+    /**
+     *Generates a successful HTTP response with a 200 OK status, wrapping the provided data and message in a ResponseStructure. This method simplifies creating standard responses for successful requests.
+     * @param data to be included in the response body. It can be of any type (T).
+     * @param message  to be included in the response, typically indicating the result or success of the operation.
+     * @return  A ResponseEntity containing a ResponseStructure object. The ResponseStructure holds the provided message and data, and the response has a 200 OK status.
+     */
+    public static<T>  ResponseEntity<ResponseStructure<T>> ok(T data, String message){
+        return success(HttpStatus.OK,message,data);
+    }
+
+    /**
+     *Generates a successful HTTP response with a 200 OK status, wrapping the provided data and message in a ResponseStructure. This method simplifies creating standard responses for successful requests.
+     * @param data to be included in the response body. It can be of any type (T).
+     * @param message  to be included in the response, typically indicating the result or success of the operation.
+     * @return  A ResponseEntity containing a ResponseStructure object. The ResponseStructure holds the provided message and data, and the response has a 200 OK status.
+     */
+    public static<T>  ResponseEntity<ResponseStructure<T>> created(T data,String message){
+        return success(HttpStatus.CREATED,message,data);
+    }
+
+    /**
+     *Generates an HTTP 404 Not Found response with a custom error message. This method is typically used to return an error when a requested resource is not found.
+     * @param message describing why the resource was not found.
+     * @return A ResponseEntity containing a SimpleErrorResponse object, which holds the provided error message. The response will have an HTTP status of 404 Not Found.
+     */
+    public static ResponseEntity<SimpleErrorResponse> notFound(String message){
+        return error(HttpStatus.NOT_FOUND,message);
     }
 }
