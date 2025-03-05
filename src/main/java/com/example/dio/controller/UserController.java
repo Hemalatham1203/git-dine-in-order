a96933aec7da167f5b06565df8accb25a798909a
Hemalatham1203 <hemalatham1203@gmail.com>
2025-03-05 08:37:08 +0530

    updated usercontroller to validate user data

--- src/main/java/com/example/dio/controller/UserController.java
+++ src/main/java/com/example/dio/controller/UserController.java
@@ -5,35 +5,73 @@ import com.example.dio.dto.request.UserRequest;
 import com.example.dio.dto.response.UserResponse;
 import com.example.dio.model.User;
 import com.example.dio.service.UserService;
+import com.example.dio.util.FieldErrorResponse;
 import com.example.dio.util.ResponseBuilder;
 import com.example.dio.util.ResponseStructure;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
+@RequestMapping("${app.base-url}")
 @RestController
 @AllArgsConstructor
-
+@Tag(name="User Controller",description = "Collection API Endpoints dealing user data.")
 public class UserController {
 
     private final UserService userService;
 
     @PostMapping("/register")
-    public ResponseEntity<ResponseStructure<UserResponse>> registerUser(@RequestBody RegistrationRequest registrationRequest) {
+    @Operation(description = """
+            The API Endpoint is used to register the User.
+            The Endpoint requires the user to select one of the specified role along with the other details.
+            """,responses = {
+            @ApiResponse(responseCode = "201",description = "user Created"),
+            @ApiResponse(responseCode = "400",description = "Invalid Input",
+            content = {
+                    @Content(schema = @Schema(implementation = FieldErrorResponse.class))
+            })
+    })
+    public ResponseEntity<ResponseStructure<UserResponse>> registerUser(@RequestBody @Valid RegistrationRequest registrationRequest) {
         UserResponse response = userService.registerUser(registrationRequest);
         return ResponseBuilder.success(HttpStatus.CREATED, "User created",response);
     }
 
     @GetMapping("/users/{userId}")
+    @Operation(description = """
+            The API endpoint used to find the user.
+            The endpoint requires the user to enter the id to find the details.
+            """,responses = {
+            @ApiResponse(responseCode = "201",description = "user found"),
+            @ApiResponse(responseCode = "400",description = "User not found",
+                    content = {
+                            @Content(schema = @Schema(implementation = FieldErrorResponse.class))
+                    })
+    })
     public ResponseEntity<ResponseStructure<UserResponse>> findUserById(@PathVariable long userId) {
         UserResponse response=userService.findUserById(userId);
         return ResponseBuilder.success(HttpStatus.OK,"user Found", response);
     }
 
-    @PutMapping("/users/update/{userId}")
+    @PutMapping("/users/{userId}")
+    @Operation(description = """
+            The API endpoint used to update the User.
+            The user required to enter the id based on the id user can update specific details such as.
+            """, responses = {
+            @ApiResponse(responseCode = "201", description = "user updated"),
+            @ApiResponse(responseCode = "400", description = "Invalid Input",
+                    content = {
+                            @Content(schema = @Schema(implementation = FieldErrorResponse.class))
+            })
+    })
+
     public ResponseEntity<ResponseStructure<UserResponse>> updateUserById(@RequestBody UserRequest userRequest,@PathVariable long userId){
         UserResponse response=userService.updateUserById(userRequest,userId);
         return ResponseBuilder.success(HttpStatus.OK,"User Updated", response);
