3114bc08845a148c16f3a5e5549f86f6e2dcd035
Hemalatham1203 <hemalatham1203@gmail.com>
2025-03-08 22:16:02 +0530

    an entity created for the table request

---
+++ src/main/java/com/example/dio/dto/request/TableRequest.java
@@ -0,0 +1,22 @@
+package com.example.dio.dto.request;
+
+import com.example.dio.enums.TableStatus;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class TableRequest {
+    @NotNull(message="Table number cannot be null ")
+    private int tableNo;
+
+    @NotNull(message="Table capacity cannot be null ")
+    @Min(value = 1, message = "Table capacity must be at least 1")
+    private int tableCapacity;
+
+    private TableStatus tableStatus;
+}
