bb317c3cc83fb1f1a56c07398442a34d0074f3d1
Hemalatham1203 <hemalatham1203@gmail.com>
2025-03-08 22:28:17 +0530

    request entity created for the restaurant

--- src/main/java/com/example/dio/dto/request/RestaurantRequest.java
+++ src/main/java/com/example/dio/dto/request/RestaurantRequest.java
@@ -2,15 +2,13 @@ package com.example.dio.dto.request;
 
 import com.example.dio.enums.DietType;
 import jakarta.persistence.Column;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Pattern;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
 import lombok.Getter;
 import lombok.Setter;
 
 import java.time.LocalTime;
 import java.util.List;
+import java.util.Set;
 
 @Getter
 @Setter
@@ -36,22 +34,17 @@ public class RestaurantRequest {
     private String email;
 
     @NotNull(message="DietType must not be null")
-    @NotBlank(message="DietType must not be blank")
+    @NotEmpty(message="DietType must not be empty")
     @Size(min = 1, message = "There must be at least one diet type")
     private List<DietType> dietTypes;
 
     @NotNull(message="CuisineType must not be null")
-    @NotBlank(message="CuisineType must not be blank")
+    @NotEmpty(message="CuisineType must not be blank")
     @Size(min = 1, message = "There must be at least one cuisine type")
-    @Pattern(regexp = "^[A-Za-z]+$")
     private List<String> cuisineTypes;
 
-    @NotNull(message = "Opening time cannot be null")
-    @Pattern(regexp="^(0?[1-9]|1[0-2]):([0-5]?[0-9]) (AM|PM)$")
     private LocalTime opensAt;
 
-    @NotNull(message = "Closing time cannot be null")
-    @Pattern(regexp = "^(0?[1-9]|1[0-2]):([0-5]?[0-9]) (AM|PM)$")
     private LocalTime closeAt;
 
 }
