fab186ba9c344e6cdf1f63ed6bbc986d900d3916
Hemalatham1203 <hemalatham1203@gmail.com>
2025-03-13 10:13:09 +0530

    updated to generate qr code

---
+++ src/main/java/com/example/dio/service/QRService.java
@@ -0,0 +1,15 @@
+package com.example.dio.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public interface QRService {
+    /**
+     * Generates a QR code as a byte array from the given URL.
+     *
+     * @param url The URL to be encoded into the QR code.
+     * @return A byte array representing the QR code image in PNG format.
+     * @throws Exception If an error occurs during QR code generation.
+     */
+    byte[] generateQRCode(String url) throws Exception;
+}
