023010c0d61155df4e5d573cbd790d3831b40b5d
Hemalatham1203 <hemalatham1203@gmail.com>
2025-03-19 11:14:07 +0530

    created to store Thymeleaf templates

--- src/main/java/com/example/dio/repository/TableRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.dio.repository;
-
-import com.example.dio.model.Restaurant_Table;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TableRepository extends JpaRepository<Restaurant_Table,Long> {
-}
