eb1e6cbd4cf9dba227b587604ddd0da681071c14
Hemalatham1203 <hemalatham1203@gmail.com>
2025-03-19 11:41:53 +0530

    updated cart item responses

--- src/main/java/com/example/dio/dto/response/CartItemResponse.java
+++ src/main/java/com/example/dio/dto/response/CartItemResponse.java
@@ -4,16 +4,19 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.List;
+
 @Getter
 @Setter
-@AllArgsConstructor
 public class CartItemResponse {
 
     private long cartId;
+
+    private FoodItemResponse foodItem;
 
     private int quantity;
 
     private double totalPrice;
 
-    private String isOrdered;
+    private boolean isOrdered;
 }
