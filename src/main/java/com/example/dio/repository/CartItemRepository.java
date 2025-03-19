5fee6d8e39ee4653c56f6f0fd796b47bcb02e1d9
Hemalatham1203 <hemalatham1203@gmail.com>
2025-03-19 11:38:26 +0530

    updated to specify the isorders status

--- src/main/java/com/example/dio/repository/CartItemRepository.java
+++ src/main/java/com/example/dio/repository/CartItemRepository.java
@@ -1,7 +1,12 @@
 package com.example.dio.repository;
 
+import com.example.dio.dto.response.CartItemResponse;
 import com.example.dio.model.CartItem;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -9,5 +14,18 @@ import java.util.List;
 
 @Repository
 public interface CartItemRepository extends JpaRepository<CartItem,Long> {
+
+    List<CartItem> findByIsOrderedAndRestaurantTable_TableId(boolean isOrdered, long tableId);
+
+    @Modifying
+    @Transactional
+    @Query("UPDATE CartItem c SET c.quantity = :newQuantity WHERE c.cartId = :cartItemId")
+    int updateQuantityByCartItemId(Long cartItemId, int newQuantity);
+
+    @Modifying
+    @Transactional
+    @Query("UPDATE CartItem c SET c.isOrdered=true WHERE c.cartId IN :cartIds")
+    void updateCartItemsIsOrdered(@Param("cartIds") List<Long> cartIds);
+    
 
 }
